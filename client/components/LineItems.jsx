c77803f2c064559f4aa22e7cf474844ed70ce14f
Zoealexandra <zoebarron@Zoes-MacBook-Pro.local>
2018-07-15 22:38:57 +1200

    adding LineItems.jsx class function

---
+++ client/components/LineItems.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import LineItem from './LineItem'
+
+class LineItems extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      addLine: false
+    }
+    this.addLine = this.addLine.bind(this)
+  }
+
+  addLine () {
+    this.setState ({
+      addLine: true
+    })
+  }
+
+ render () {
+   return (
+    <div className='lineItems'>
+    <LineItem /> 
+    <button onSubmit={this.addLine}>Add Line Item</button>
+    {this.addLine && <LineItem />} 
+  </div>
+   )
+ }
+
+}
+
+export default LineItems
