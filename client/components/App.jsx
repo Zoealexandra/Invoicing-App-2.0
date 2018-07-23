eecd46e53d890e743e86ea21bcc25a4f9fe6b265
Zoealexandra <zoebarron@Zoes-MacBook-Pro.local>
2018-07-23 23:06:41 +1200

    adding react router and two routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+
 import InvoiceDetails from './InvoiceDetails';
 import ViewAllInvoices from './ViewAllInvoices'
 
+import {HashRouter as Router, Route} from 'react-router-dom'
+
 const App = () => (
-  <div className='app'>
-    <InvoiceDetails />
-    <ViewAllInvoices />
-  </div>
+  <Router>
+    <div className='app'>
+      <Route path='/addInvoice' component={InvoiceDetails} />
+      <Route exact path='/viewinvoices' component={ViewAllInvoices} />
+    </div>
+  </Router>
 )
 
 export default App
